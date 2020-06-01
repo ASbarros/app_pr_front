5385b64f28992ff55eea52509ee71ba722b774f1
asbarros <anderson.barros0298@gmail.com>
2020-05-31 21:47:57 -0300

    concluindo a tela

--- pr/lib/pages/home.dart
+++ pr/lib/pages/home.dart
@@ -11,19 +11,26 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _currValue = 1;
+  int _currValue;
+  bool _selected;
 
   Question question;
 
-  @override
-  void initState() {
+  _init() {
+    _currValue = -1;
+    _selected = false;
     question = Question();
-    super.initState();
     Question.get().then((value) {
       setState(() {
-        question.description = value.description;
+        question = value;
       });
     });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _init();
   }
 
   @override
@@ -32,98 +39,53 @@ class _MyHomePageState extends State<MyHomePage> {
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.only(top: 23.0),
-          child: Column(
+      body: Container(
+        padding: EdgeInsets.only(top: 23.0),
+        child: Card(
+          elevation: 2.0,
+          color: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(8.0),
+          ),
+          child: ListView(
             children: <Widget>[
-              Stack(
-                alignment: Alignment.topCenter,
-                overflow: Overflow.visible,
-                children: <Widget>[
-                  Card(
-                    elevation: 2.0,
-                    color: Colors.white,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
-                    ),
-                    child: Container(
-                      width: 300.0,
-                      height: 330.0,
-                      child: Column(
-                        children: <Widget>[
-                          Padding(
-                              padding: EdgeInsets.only(
-                                  top: 20.0,
-                                  bottom: 20.0,
-                                  left: 25.0,
-                                  right: 25.0),
-                              child: Text(question.description)),
-                          Container(
-                            width: 250.0,
-                            height: 1.0,
-                            color: Colors.grey[400],
-                          ),
-                          Padding(
-                            padding: EdgeInsets.only(
-                                top: 20.0,
-                                bottom: 20.0,
-                                left: 25.0,
-                                right: 25.0),
-                            child: Column(
-                              children: <Widget>[
-                                Row(
-                                  children: <Widget>[
-                                    Radio(
-                                      groupValue: _currValue,
-                                      onChanged: (int i) =>
-                                          setState(() => _currValue = i),
-                                      value: 1,
-                                    ),
-                                    Text(question.answers[0].description)
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Radio(
-                                      groupValue: _currValue,
-                                      onChanged: (int i) =>
-                                          setState(() => _currValue = i),
-                                      value: 2,
-                                    ),
-                                    Text('data')
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Radio(
-                                      groupValue: _currValue,
-                                      onChanged: (int i) =>
-                                          setState(() => _currValue = i),
-                                      value: 3,
-                                    ),
-                                    Text('data')
-                                  ],
-                                ),
-                                Row(
-                                  children: <Widget>[
-                                    Radio(
-                                      groupValue: _currValue,
-                                      onChanged: (int i) =>
-                                          setState(() => _currValue = i),
-                                      value: 4,
-                                    ),
-                                    Text('data')
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ],
-                      ),
+              Container(
+                child: Text(question.description),
+              ),
+              Container(
+                height: 225.0,
+                child: ListView.builder(
+                  itemCount: question.answers.length,
+                  itemBuilder: (context, index) => Card(
+                    color: question.answers[index].color,
+                    elevation: 2.5,
+                    child: Row(
+                      children: <Widget>[
+                        Radio(
+                          groupValue: _currValue,
+                          activeColor: Colors.black,
+                          onChanged: (int i) {
+                            if (!_selected) {
+                              _selected = !_selected;
+                              question.answers[index].right
+                                  ? question.answers[index].color = Colors.green
+                                  : question.answers[index].color = Colors.red;
+                              setState(() => _currValue = i);
+                            }
+                          },
+                          value: index,
+                        ),
+                        Text(question.answers[index].description),
+                      ],
                     ),
                   ),
-                ],
+                ),
+              ),
+              Center(
+                child: RaisedButton(
+                  child: Text('Próxima'),
+                  onPressed: () => _init(),
+                ),
               ),
             ],
           ),
